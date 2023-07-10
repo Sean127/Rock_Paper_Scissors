3e28651e0ea6755a4ec3ddbd725a7aa33fd73153
Sean127 <sdoran401@gmail.com>
2023-07-10 10:39:02 +0000

    Add answer validation

--- run.py
+++ run.py
@@ -12,33 +12,57 @@ def runGame():
     while player == False:
         print("Welcome to Rock, Paper, Scissors!")
         print('Please select either "Rock", "Paper" or "Scissors"')
+        print('To quit the game, type "quit"')
         player = input("Choose Wisely!: ")
         player_option.append(player)
 
-        if player == cpu:
-            print("You chose",player,". CPU chose",cpu,".")
-            print("Its a TIE!!!")
-        elif player == "Rock":
-            if cpu == "Paper":
-                print("You chose Rock. CPU chose Paper.")
-                print("You lose!")
-            else:
-                print("You chose Rock. CPU chose Scissors.")
-                print("You win!")
-        elif player == "Paper":
-            if cpu == "Scissors":
-                print("You chose Paper. CPU chose Scissors.")
-                print("You lose!")
-            else:
-                print("You chose Paper. CPU chose Rock.")
-                print("You win!")
-        elif player == "Scissors":
-            if cpu == "Rock":
-                print("You chose Scissors. CPU chose Rock.")
-                print("You lose!")
-            else:
-                print("You chose Scissors. CPU chose Paper.")
-                print("You win!")
+        if player == "quit":
+            break
+
+        if validate_option(player_option) == True:
+            if player == cpu:
+                print("You chose",player,". CPU chose",cpu,".")
+                print("Its a TIE!!!\n")
+            elif player == "Rock":
+                if cpu == "Paper":
+                    print("You chose Rock. CPU chose Paper.")
+                    print("You lose!\n")
+                else:
+                    print("You chose Rock. CPU chose Scissors.")
+                    print("You win!\n")
+            elif player == "Paper":
+                if cpu == "Scissors":
+                    print("You chose Paper. CPU chose Scissors.")
+                    print("You lose!\n")
+                else:
+                    print("You chose Paper. CPU chose Rock.")
+                    print("You win!\n")
+            elif player == "Scissors":
+                if cpu == "Rock":
+                    print("You chose Scissors. CPU chose Rock.")
+                    print("You lose!\n")
+                else:
+                    print("You chose Scissors. CPU chose Paper.")
+                    print("You win!\n")
+
+        player = False
+        cpu = options[randint(0,2)]
+
+
+def validate_option(player_option):
+    try:
+        [option for option in player_option]
+        if ("Rock" or "Paper" or "Scissors") not in player_option:
+            player_option.clear()
+            raise ValueError(
+            "Invalid answer. Please try again.\n"
+            )
+
+    except ValueError as e:
+        print(f"Invalid data: {e}")
+        return False
+        
+    return True
 
 def main():
     runGame()
